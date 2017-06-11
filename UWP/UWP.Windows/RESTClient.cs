1a12dc6631cdf00854ef53bcf266813cb0ceef06
Kamil Hliwa <elathorn@gmail.com>
2017-06-11 19:19:56 +0200

    Main, about and weather page. Weather page only with hardcoded location

---
+++ UWP/UWP.Windows/RESTClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace UWP
+{
+    class RESTClient
+    {
+        private string APIBaseAddress = "http://api.openweathermap.org/data/2.5/";
+        private string weather = "weather?q=";
+        private string apiKey = "52ed52304ece1cfeaee3e237bf6c2094";
+        private string units = "&units=metric";
+        public string getWeatherForLocation(string location)
+        {
+            var uri = new Uri(APIBaseAddress + weather + location + units);
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("x-api-key", apiKey);
+            HttpResponseMessage response;
+            response = client.GetAsync(uri).Result;
+            response.EnsureSuccessStatusCode();
+            var content = response.Content.ReadAsStringAsync().Result;
+            return content;
+        }
+    }
+}
---
+++ UWP/UWP.Windows/WeatherInfo.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP
+{
+    class WeatherInfo
+    {
+        [JsonProperty("name")]
+        public string city { get; set; }
+
+        [JsonProperty("main")]
+        WeatherInfoInside weatherInfoInside { get; set; }
+
+        [JsonProperty("weather")]
+        List<Weather> weather { get; set; }
+
+        public WeatherInfo() { }
+
+        public string getHumidityAsString()
+        {
+            return String.Format("{0}%", weatherInfoInside.humidity).ToString();
+        }
+
+        public string getTemperatureAsString()
+        {
+            return String.Format("{0} °C", weatherInfoInside.temperature);
+        }
+
+        public string getWeatherDescription()
+        {
+            return weather[0].description;
+        }
+    }
+
+    class WeatherInfoInside
+    {
+        [JsonProperty("temp")]
+        public long temperature { get; set; }
+
+        [JsonProperty]
+        public int humidity { get; set; }
+    }
+
+    class Weather
+    {
+        [JsonProperty("description")]
+        public string description { get; set; }
+        Weather()
+        {
+
+        }
+    }
+}
